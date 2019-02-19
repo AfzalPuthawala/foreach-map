466e1d6a2e76b9b86335a5bcf78ce947bc2b7dc8
AfzalPuthawala <47767431+AfzalPuthawala@users.noreply.github.com>
2019-02-19 13:36:57 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,11 @@
+var arr2=[10,8,6,4,2,9,7,5,3,1];
+
+var new_arr = Arr2.map((val,i,Arr) => {
+										for(var j=0;j<0;j++)
+										if(Arr2[i] < Arr2[j]){
+															var x = Arr2[i];
+															Arr2[i] = Arr2[j];
+															Arr2[j] =x;
+														 }
+									  });				 
+cosole.log(Arr2);
